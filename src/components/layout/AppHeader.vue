05a4f93bd4ba40548efa90b55350f3e73e6da0e4
beeeteR <130251761+beeeteR@users.noreply.github.com>
2024-11-08 13:36:14 +0400

    rebuild project + project setup
    Пересобрал, т.к. изначально установил немного не то, что нужно было.
    Разбил по папкам/файлам, добавил sass в конфиг vite

---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header class="app__header">
+    <div class="app__logo-wrapper">
+      <img src="@/assets/image/logo.png" alt="logo" class="app__logo">
+    </div>
+    <nav class="header__nav">
+      <router-link to="/"  class="header__nav-item --p2"/>
+      <router-link to="/info" class="header__nav-item --p2"/>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss">
+.app__header {
+  .header__nav {
+    &-item {
+      color: map-get($colors, 'grey')
+    }
+  }
+}
+
+.app__logo-wrapper {
+  .app__logo {
+
+  }
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@use "@/assets/style/main.scss" as *;',
+        api: 'modern'
+      },
+    },
+  },
+})
